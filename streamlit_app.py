b364cc4a5b59ee88b49bb9af51b919a54bb74cf2
aqiilamumtaza26-crypto <aqiilamumtaza26@gmail.com>
2026-05-19 11:30:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ st.badge("Success", icon=":material/check:", color="green")
 st.markdown(
     ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
 )
+
+import streamlit as st
+
+st.balloons()
